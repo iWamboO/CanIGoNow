59a6b336fb630a4f7de15ca0e488e5a2dedf053a
Mathias <ms400@hdm-stuttgart.de>
2020-02-18 11:42:33 +0100

    Added basic logic for calculating time

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 var app = new Vue({
     template: 
     `
-     <h1>{{ name }}</h1>
+    <div>
+ 
+        <p>How long do you wanna work?</p>
+        <input type="number" v-model="worktimeValueInputHours">
+        <input type="number" v-model="worktimeValueInputMinutes">
+        <p>Timestamp in</p>
+        <input type="number" v-model="timestampValueInputHours">
+        <input type="number" v-model="timestampValueInputMinutes">
+        <p>Lunchtime</p>
+        <input type="number" v-model="lunchtimeValueInput">
+
+        <p>{{youCanGoAt}}</p>
+
+    </div>
     `,
     el: '#app',
     data() {
         return {
-            name: "hello there"
+            worktimeValueInputHours: 0,
+            worktimeValueInputMinutes: 0,
+            timestampValueInputHours: 0,
+            timestampValueInputMinutes: 0,
+            lunchtimeValueInput: 30,
+        }
+    },
+    computed: {
+        youCanGoAt(){
+
+            const worktimeTimeCalc = this.worktimeValueInputHours * 3600000 + this.worktimeValueInputMinutes * 60000
+            const timestampTimeCalc = this.timestampValueInputHours * 3600000 + this.timestampValueInputMinutes * 60000
+            const lunchtimeValueInputCalc = this.lunchtimeValueInput * 60000
+
+            return new Date(worktimeTimeCalc + timestampTimeCalc + lunchtimeValueInputCalc).toLocaleTimeString();
         }
     }
+
 }) 
