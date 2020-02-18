d3be0eaaabf77183c8a9b5456e96db9b8b0e5e0e
Mathias <ms400@hdm-stuttgart.de>
2020-02-18 15:20:08 +0100

    Styling

--- app.js
+++ app.js
@@ -10,23 +10,46 @@ var app = new Vue({
         <div class="app-body">
             
             <div class="app-body-element">
-                <p>How long do you wanna work?</p>
-                <input type="number" v-model="worktimeValueInputHours" min="0" max="10">
-                <input type="number" v-model="worktimeValueInputMinutes" min="0" max="60">
+                <div class="app-body-element-description">
+                    <p>How long do you wanna work?</p>
+                </div>
+                <div class="app-body-element-input">
+                    <input type="number" v-model="worktimeValueInputHours" min="0" max="10">
+                    <span>:</span>
+                    <input type="number" v-model="worktimeValueInputMinutes" min="0" max="60">
+                </div>
             </div>
 
             <div class="app-body-element">
-                <p>Timestamp in</p>
-                <input type="number" v-model="timestampValueInputHours" min="6" max="17">
-                <input type="number" v-model="timestampValueInputMinutes" min="0" max="60">
+                <div class="app-body-element-description">
+                    <p>Timestamp in</p>
+                </div>
+                <div class="app-body-element-input">
+                    <input type="number" v-model="timestampValueInputHours" min="6" max="17">
+                    <span>:</span>
+                    <input type="number" v-model="timestampValueInputMinutes" min="0" max="60">
+                </div>
             </div>
             
             <div class="app-body-element">
-                <p>Lunchtime</p>
-                <input type="number" v-model="lunchtimeValueInput" min="0" max="60">
+                <div class="app-body-element-description">
+                    <p>Lunchtime</p>
+                </div>
+                <div class="app-body-element-input">
+                    <input type="number" v-model="lunchtimeValueInputHours" min="0" max="10">
+                    <span>:</span>
+                    <input type="number" v-model="lunchtimeValueInputMinutes" min="0" max="60">
+                </div>
             </div>
 
-            <p>{{youCanGoAt}}</p>
+            <div class="app-body-element">
+                <div class="app-body-element-description">
+                    <p>You Can Go At</p>
+                </div>
+                <div class="app-body-element-input">
+                    <p>{{youCanGoAt}}</p>
+                </div>
+            </div>
         </div>
 
     </div>
@@ -38,7 +61,8 @@ var app = new Vue({
             worktimeValueInputMinutes: 0,
             timestampValueInputHours: 6,
             timestampValueInputMinutes: 0,
-            lunchtimeValueInput: 30,
+            lunchtimeValueInputHours: 0,
+            lunchtimeValueInputMinutes: 30,
         }
     },
     computed: {
@@ -46,7 +70,7 @@ var app = new Vue({
 
             const worktimeTimeInMilliseconds = this.worktimeValueInputHours * 3600000 + this.worktimeValueInputMinutes * 60000
             const timestampTimeInMilliseconds = this.timestampValueInputHours * 3600000 + this.timestampValueInputMinutes * 60000
-            const lunchtimeValueInputInMilliseconds = this.lunchtimeValueInput * 60000
+            const lunchtimeValueInputInMilliseconds = this.lunchtimeValueInputHours * 3600000 + this.lunchtimeValueInputMinutes * 60000
 
             return new Date(worktimeTimeInMilliseconds + timestampTimeInMilliseconds + lunchtimeValueInputInMilliseconds - 3600000).toLocaleTimeString(navigator.language, {hour: '2-digit',minute:'2-digit'});
         }
